936cc3feb6bc8cf83d06272deb4c572956ee2add
moz <moz@local>
2014-05-13 00:12:33 +0200

    non-functioning test

---
+++ busstalk/src/unittest/testServer.py
@@ -0,0 +1,26 @@
+'''
+Created on May 13, 2014
+
+@author: moz
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
