3d4c1028992cdb5dc142c662f45497f956e57a9d
moz <moz@local>
2014-05-23 21:02:47 +0200

    Made bsClient compatible with "with" changed test to use different random ports All test pass - cleanup time

--- busstalk/src/bsClient.py
+++ busstalk/src/bsClient.py
@@ -36,6 +36,18 @@ class bsClient(object):
         if not self._receive().split(' ')[0] == 'WELCOME':
             raise IOError( 'Unknown response' )
     
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, errortype, value, traceback): 
+        print >> sys.stderr, "Closing socket"
+        self._sock.shutdown(socket.SHUT_RDWR)
+        while True:
+            data = self._sock.recv(16)
+            if not data:
+                break
+        self._sock.close()
+    
     def _send(self, text ): 
         ''' send the specified text - newline is automaticly appended, if needed
         '''
@@ -55,6 +67,32 @@ class bsClient(object):
                 break
                 
         return retval[:-1]
+
+    def _sendCommand(self, command ):
+        self._send(command)
+        data = self._receive().split(' ')
+        
+        # first word is success or not
+        result = data.pop(0)
+        if result != "OK":
+            raise IOError( "Command '%s' failed: %d"%(command, data))
+        
+        # next word is the command echo
+        cmdEcho = data.pop(0)
+        if cmdEcho != command:
+            raise IOError( "Command echo failed: Got '%s' exspected '%s'"%(cmdEcho, command))
+        
+        # the rest is the result
+        return data
+
+    def getDevices(self):
+        ''' Send a request for the list of devices
+            ie. send "DEVICES" command
+        '''
+        cmd = "DEVICES"
+        retval = self._sendCommand(cmd)
+        return retval
+        
             
 # the program starts from here
 if __name__ == "__main__":
--- busstalk/src/unittests/testClient.py
+++ busstalk/src/unittests/testClient.py
@@ -15,21 +15,29 @@ import socket, sys, time
 
 from bsClient import bsClient
 
-tcpPort = randint(10000,11000)
 srvHost = 'localhost'
 srvErrorMsg = 'ERROR'
 
-def runMinimalServer( ReturnData = ("WELCOME\n", )  ):
+def runMinimalServer( tcpPort, ReturnData = ("WELCOME\n", )  ):
     ''' create a server and return the strings from the array, one line at the time 
         as response to (un-parsed) received data
         NB: Remember newlines!
     '''
+    
+    print >> sys.stderr, "%s: starting"%(threading.current_thread().name )
+        
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     # Bind the socket to the address given on the command line
     server_address = ('', tcpPort)
-    sock.bind(server_address)
+    
+    try:
+        sock.bind(server_address)
+    except IOError, e:
+        print >> sys.stderr, "%s: Not creating server!! %s"%(threading.current_thread().name, e.message)
+        return
+        
     
     sock.listen(1)
     connection, client_address = sock.accept()  # @UnusedVariable
@@ -39,19 +47,20 @@ def runMinimalServer( ReturnData = ("WELCOME\n", )  ):
         while True:
             data = connection.recv(16)
             if len(data) < 1:
-                print >> sys.stderr, "zero data"
+                print >> sys.stderr, "%s: zero data"%threading.current_thread().name
                 connection.sendall( srvErrorMsg + " zero data received\n")
                 break
-            print >> sys.stderr, "recv: %s"%data
+            print >> sys.stderr, "%s: recv: %s"%(threading.current_thread().name, data)
             if len(data) < 1 or data[-1] != '\n':
                 continue
             else:
-                print >> sys.stderr, "sending: %s" %retval
+                print >> sys.stderr, "%s: sending: %s" %(threading.current_thread().name, retval)
                 connection.sendall(retval) # just echo stuff
                 break
             
+    connection.shutdown( socket.SHUT_RDWR )
     connection.close()
-    print >> sys.stderr, "done"
+    print >> sys.stderr, "%s: done"%threading.current_thread().name
 
 class Test(unittest.TestCase):
 
@@ -66,37 +75,43 @@ class Test(unittest.TestCase):
     def testBadConnect(self):
         ''' test connecting to non-existing server
         '''        
+        tcpPort = randint(10000,11000)
         self.assertRaises( IOError, bsClient, 'bogusserver', tcpPort  )
         
     
     def testGoodConnect(self):
         ''' test connecting to localhost
         '''        
-        t = threading.Thread(target=runMinimalServer, name="TestClientThread" )
+        tcpPort = randint(10000,11000)
+
+        t = threading.Thread(target=runMinimalServer, name="__testGoodConnect", args=(tcpPort,) )
         t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
         t.start()
         time.sleep(0.1)
          
-        c = bsClient( srvHost, tcpPort  )  # @UnusedVariable
+        with bsClient( srvHost, tcpPort  ) as c:  # @UnusedVariable
+            pass
+        
         t.join(3)
          
      
-#     def testGetDevices(self):
-#         ''' test connecting to localhost
-#         '''        
-#         t = threading.Thread(target=runMinimalServer, 
-#                              name="TestClientThread", args=(('WELCOME\n', 'OK DEVICES EEPROM LED\n'), ) )
-#         t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
-#         t.start()
-#          
-#         c = bsClient( srvHost, tcpPort  )  # @UnusedVariable
-#         devs = c.getDevices()
-#         
-#         self.assertEqual( devs, ['EEPROM', 'LED'])
-# 
-#         
-#         t.join(3)
-#          
+    def testGetDevices(self):
+        ''' test connecting to localhost
+        '''        
+        tcpPort = randint(10000,11000)
+        
+        t = threading.Thread(target=runMinimalServer, 
+                             name="__testGetDevices", args=(tcpPort, ('WELCOME\n', 'OK DEVICES EEPROM LED\n'), ) )
+        t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
+        t.start()
+        time.sleep(0.1)
+          
+        with bsClient( srvHost, tcpPort  ) as c:  # @UnusedVariable
+            devs = c.getDevices()
+            self.assertEqual( devs, ['EEPROM', 'LED'])
+ 
+        t.join(3)
+          
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testCreate']
