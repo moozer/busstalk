e3f2101a3ed1dc305fae056bb7084d2765a2e88b
moz <moz@local>
2014-09-02 16:01:40 +0200

    Added timeout

--- busstalk/src/bsServer.py
+++ busstalk/src/bsServer.py
@@ -8,6 +8,7 @@ Most of this comes from http://pymotw.com/2/socket/tcp.html
 
 import socket
 import sys
+import time
 
 
 class bsServer(object):
@@ -15,60 +16,155 @@ class bsServer(object):
     classdocs
     '''
 
+    _devices = ['EEPROM', 'LED']
+
+
     def __init__(self, port = 10000):
         '''
         Constructor
         '''
         self._ConnectionCount = 0
         self._tcpPort = port
+        self._cmdCount = 0
     
         # Create a TCP/IP socket
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
         # Bind the socket to the address given on the command line
         server_address = ('', self._tcpPort)
-        self.sock.bind(server_address)
-        print >>sys.stderr, 'Starting BS server up on %s port %s' % self.sock.getsockname()
+        self._sock.bind(server_address)
+        print >>sys.stderr, 'Starting BS server up on %s port %s' % self._sock.getsockname()
         
+    def _send(self, text ): 
+        ''' send the specified text - newline is automaticly appended, if needed
+        '''
+        if text[-1] != '\n':
+            text += '\n'
+        self._curConnection.sendall( text ) 
         
-    def runOnce(self):
+    def _receive( self, timeoutcount = 20, waittime = 0.1 ):
+        ''' Merges all received data until newline is received.
+        '''
+        # TODO: what happens if newline is not the last char?
+        retval = ''
+        while True:
+            data = self._curConnection.recv( 16 )
+            
+            # if no data, just try again
+            if len(data) > 0:
+                retval += data
+                
+                # if newline at the end, we have a command
+                if retval[-1] == '\n':
+                    break
+
+            else:
+                time.sleep( waittime )
+                timeoutcount -= 1
+                print >> sys.stderr, "(%d) waiting: %d"%(self._tcpPort, timeoutcount)
+                
+            # if timeout
+            if timeoutcount < 1:
+                return "TIMEOUT"
+            
+        return retval[:-1]
+    
+    def _firstContact(self, command):
+        expCmd = "GREETINGS"
+        if command != "GREETINGS":
+            print >> sys.stderr, "First contact failed: got '%s' expected '%s'"%(command, expCmd)
+            self._send("ERROR got '%s' expected '%s'"%(command, expCmd))
+            return False
+        
+        self._send('WELCOME')
+        return True
+    
+    def _sendUnknownCommand(self, command):
+        ''' catch-all function to handle misspelling and the like '''
+        print >> sys.stderr, "Unknown command:'%s'"%(command, )
+        self._send("ERROR UNKOWN COMMAND '%s'"%(command, ))
+        return True 
+    
+    def _sendDevices(self, command):
+        self._send("OK %s %s"%(command, ' '.join(self._devices)))
+        return True
+    
+    def _parse(self, command):
+        self._cmdCount += 1
+        
+        if self._cmdCount == 1: # first command
+            return self._firstContact( command )
+        
+        if command == "DEVICES": # request device list
+            return self._sendDevices( command )
+        
+        return self._sendUnknownCommand(command)
+    
+    
+    def runOnce(self ):
         ''' the main function to handle incoming data
         '''
-        self.sock.listen(1)
-        
-        print >>sys.stderr, 'waiting for a connection'
-        connection, client_address = self.sock.accept()
-        self._ConnectionCount += 1
-        try:
-            print >>sys.stderr, 'client connected:', client_address
-            while True:
-                data = connection.recv(16)
-                print >>sys.stderr, 'received "%s"' % data
-                if data:
-                    connection.sendall(data) # just echo stuff
-                else:
-                    return True
-        finally:
-            connection.close()
+        self.runMultiple(1)
+#         self._sock.listen(1)
+#         command = ""
+#         
+#         print >>sys.stderr, 'waiting for a connection'
+#         connection, client_address = self._sock.accept()
+#         print >>sys.stderr, 'client connected:', client_address
+#         self._ConnectionCount += 1
+#         try:
+#             command = self._receive()
+#             return self._parse(command)
+#         except IOError, e: 
+#             print >> sys.stderr, "Error caught receiving data so closing connection: %s"%e
+#         finally:
+#             connection.shutdown()
+#             connection.close()
             
-    def runMultiple(self, count = 1):
+    def runMultiple(self, count = 1, runForever = False):
         ''' handles multiple consecutive connections
         '''
-        while count > 0:
-            if not self.runOnce():
-                return False
+        self._sock.listen(1)
+        command = ""
+        notQuit = True
+        
+        try:
+            print >>sys.stderr, '(%d) waiting for a connection'%self._tcpPort
             
-            count -= 1
+            self._curConnection, client_address = self._sock.accept()
+            print >>sys.stderr, "(%d) client connected: %s"%(self._tcpPort, client_address)
+        
+            while runForever or count > 0:
+                self._ConnectionCount += 1
+                count -= 1
+                try:
+                    command = self._receive()
+                    print >> sys.stderr, "command: %s"%command
+                    notQuit = self._parse(command)
+                    if not notQuit:
+                        break                
+                except IOError, e: 
+                    print >> sys.stderr, "(%d) IOError caught receiving/sending data - closing connection: %s"%(self._tcpPort, e)
+                    break           
+
+        finally:
+            print >>sys.stderr, "(%d) shutdown and close"%self._tcpPort
+            #self._curConnection.shutdown(socket.SHUT_RDWR)
+            self._curConnection.close()
+            
+        if not notQuit:
+            return
+        
+        # TODO: this is bad
+        if count > 0:
+            self.runMultiple(count, runForever)
         
     def start(self):
         ''' server will sit and wait for connection from client
             and then process it.
         '''
-        self.sock.listen(1)
-        
-        while True:
-            if not self.runOnce():
-                break
+        self.runMultiple( count = 0, runForever=True)
+        print >> sys.stderr, "Shutdown as per requested"
 
     # --- 
     # getters 
--- busstalk/src/unittests/testServer.py
+++ busstalk/src/unittests/testServer.py
@@ -13,11 +13,10 @@ from random import randint
 parentdir = os.path.dirname(os.path.dirname( '..'))
 os.sys.path.insert(0,parentdir)
 
-quitMessage = "QUIT"
-tcpPort = randint(10000,11000)
-testCount = 5
+firstMessage = "GREETINGS\n"
+testCount = 3
 
-class TestClass(unittest.TestCase):
+class TestServer(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -26,52 +25,116 @@ class TestClass(unittest.TestCase):
         pass
 
     def testCreate(self):
+        ''' test simple class instatiation '''
+        tcpPort = randint(10000,11000)
         s = bsServer( port = tcpPort)  
         self.assertEqual( s.getConnectionCount(), 0 )
         self.assertEqual( s.getTcpPort(), tcpPort)
         
-    
-class TestConnection(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
     def testConnect(self):
+        ''' test tcp connection '''
+        tcpPort = randint(10000,11000)        
         bss = bsServer(port = tcpPort)  
         t = threading.Thread(target=bss.runOnce, name="TestServerThread")
         t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
         t.start()
         time.sleep(0.1)
-
+ 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
         s.connect(('localhost', tcpPort))
-        s.send( quitMessage )
+        s.send( firstMessage )
         s.close()
-        
+         
         t.join(3)
         self.assertEqual( bss.getConnectionCount(), 1)
         pass
-
+  
     def testMultipleConnect(self):
+        tcpPort = randint(10000,11000)
         bss = bsServer(port = tcpPort)  
         t = threading.Thread(target=bss.runMultiple, name="TestServerThread", args=(testCount,) )
         t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
         t.start()
         time.sleep(0.1)
-         
-        for i in range(0, testCount ):  
+           
+        for i in range(0, testCount ):
+            print "start connect %d"%i
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(30)
             s.connect(('localhost', tcpPort))
-            s.send( "connection %d"%i )
+            print "sending %s"%firstMessage
+            s.send( firstMessage )
+            #s.send( "connection %d\n"%i )
+            time.sleep(0.1)
+            s.shutdown( socket.SHUT_RDWR)
+            time.sleep(0.1)
             s.close()
-        
+            print "end %d"%i
+          
         t.join(3)
+        #t.join()
         self.assertEqual( bss.getConnectionCount(), testCount )
         pass
-
+ 
+    def testQuit(self):
+        tcpPort = randint(10000,11000)
+        bss = bsServer(port = tcpPort)  
+        t = threading.Thread(target=bss.start, name="TestServerThread" )
+        t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
+        t.start()
+        time.sleep(0.1)
+  
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(50)
+        s.connect(('localhost', tcpPort))
+          
+        # doing greeting
+        s.send( "GREETINGS\n" )
+        fromSrv = ''
+        while True:
+            data = s.recv(16)
+            self.assertTrue(data)
+            fromSrv += data
+            if len(fromSrv) > 1 and fromSrv[-1] == '\n':
+                break
+        self.assertEqual( fromSrv, 'WELCOME\n')
+  
+        s.send( "DEVICES\n" )        
+        fromSrv = ''
+        while True:
+            data = s.recv(16)
+            self.assertTrue(data)
+            fromSrv += data
+            if len(fromSrv) > 1 and fromSrv[-1] == '\n':
+                break
+        self.assertEqual( fromSrv, 'OK DEVICES EEPROM LED\n')
+          
+        s.close()
+      
+        t.join(3)
+        pass
+    
+        
+# class TestConnection(unittest.TestCase):
+# 
+#     def setUp(self):
+#         self.tcpPort = randint(10000,11000)
+#         bss = bsServer(port = self.tcpPort)  
+#         self.t = threading.Thread(target=bss.start, name="TestServerThread" )
+#         self.t.daemon = False  # thread dies when main thread (only non-daemon thread) exits.
+#         self.t.start()
+#         time.sleep(0.1)
+# 
+#     def tearDown(self):
+#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         s.connect(('localhost', self.tcpPort))
+#         s.send( firstMessage )
+#         s.close()
+# 
+#     def testQuit(self):
+#         
+#         pass
 
 
 if __name__ == "__main__":
