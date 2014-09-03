186a878fe0dd9f724236cab8d1fb1cc15f885cbc
moz <moz@local>
2014-09-03 12:39:53 +0200

    Added chatMod class

---
+++ busstalk/src/chatMod.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 3, 2014
+
+@author: moz
+'''
+
+class chatMod(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ busstalk/src/unittests/testChatMod.py
@@ -0,0 +1,27 @@
+'''
+Created on Sep 3, 2014
+
+@author: moz
+'''
+import unittest
+from chatMod import chatMod
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testConstruct(self):
+        c = chatMod()
+        
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testConstruct']
+    unittest.main()
